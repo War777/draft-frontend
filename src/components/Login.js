6d328df316ccdc29b7f1bdc79791c321fd7bc515
Oscar Mendoza <oscar.resendiz@freeagentsopftware.com>
2019-10-16 03:54:43 -0500

    ab

--- src/components/Login.js
+++ src/components/Login.js
@@ -25,7 +25,7 @@ class Login extends Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    let url = 'http://localhost/draft-backend/public/api/login';
+    let url = 'http://localhost:8080/draft-backend/public/api/login';
     if (this.state.email === '' || this.state.password === '') {
       alert('Please enter your E-mail and password.');
       return null;
